ea2771ae5cbe3a5c450fc4d85733ba18c765dd62
Rachel Grier <rgrier104@gmail.com>
2019-08-13 02:55:29 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Rachel."
+  end
+
+  get '/hometown' do
+    "My hometown is Ellicott City, MD."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Dreams."
+  end
+
 end
